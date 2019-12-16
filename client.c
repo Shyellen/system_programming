2b1ddf020a1d7265db17168cb97c0f40f94ee234
sohyeonyun <57862001+sohyeonyun@users.noreply.github.com>
2019-12-16 22:11:15 +0900

    Add files via upload
    서버 - 멀티쓰레드로 만들어 여러 클라이언트와 연결 & 데이터 송수신 가능
    보충할 점 : command 입력을 먼저 받아서인지 서버에서 연결 종료를 할 수 없음.

--- client.c
+++ client.c
@@ -5,20 +5,23 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <ctype.h>
+#include <pthread.h>
 
+#define MAXARGS	20
 #define oops(msg)		{perror(msg); exit(1);}
 
-void sanitize(char *str);
+void *send_msg(void *arg);
+void *recv_msg(void *arg);
+int rpipe[2];
 
 int main(int ac, char *av[])
 {
 	int sock;					/* the socket and fd	*/
 	struct sockaddr_in serv_adr;/* the number to call	*/
-	FILE *sock_fpi, *sock_fpo;
-	FILE *pipe_fp;
-	char dirname[BUFSIZ];
-	char command[BUFSIZ];
-	int str_len, c;
+	char combuf[BUFSIZ];
+	char *command[MAXARGS];
+	int str_len, numargs=0;
+	pthread_t snd_thread, rcv_thread;
 
 	if(ac != 3) {
 		printf("[USAGE] %s <IP> <PORT>\n", av[0]);
@@ -36,39 +39,71 @@ int main(int ac, char *av[])
 
 	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) != 0)
 		oops("connect() error");
-   
-	if((sock_fpi = fdopen(sock, "r")) == NULL)
-		oops("fdopen reading");
-	if(fgets(dirname, BUFSIZ-5, sock_fpi) == NULL)
-		oops("reading dirname");
+	printf("[NOTICE] Successfully connected.\n");
 
-	sanitize(dirname);
+	if(pipe(rpipe) == -1)
+		oops("pipe() error");
 
-	if((sock_fpo = fdopen(sock, "w")) == NULL)
-		oops("fdopen writing");
+	if(dup2(rpipe[0], 0) == -1)
+		oops("dup2() 0 error");
+	close(rpipe[0]);
+	if(dup2(rpipe[1], 1) == -1)
+		oops("dup2() 1 error");
+	close(rpipe[1]);	
 
-	sprintf(command, "ls %s", dirname);
-	if((pipe_fp = popen(command, "r")) == NULL)
-		oops("popen() error");
-	
-	while((c=getc(pipe_fp)) != EOF)
-		putc(c, sock_fpo);
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+	pthread_join(snd_thread, NULL);
+	pthread_join(rcv_thread, NULL);
 
-	pclose(pipe_fp);
-	fclose(sock_fpo);
-	fclose(sock_fpi);
-	close(sock);
+	close(sock);  
 	return 0;
 }
 
-void sanitize(char *str) {
-	char *src, *dest;
-	
-	for(src=dest=str; *src; src++) {
-		if(*src == '/' || isalnum(*src))
-			*dest++ = *src;
-	}
-	*dest = '\0';
+void *send_msg(void *arg)
+{
+	int sock=*((int*)arg);
+	int str_len;
+	char result[BUFSIZ];
 
-	return;
+	while(1) {
+		str_len = read(0, result, BUFSIZ);
+		if( str_len == -1)
+			oops("read() error");
+		result[str_len] = 0;
+		if( write(sock, result, str_len) == -1)
+			oops("write() error");
+	}
+	return NULL;
+}
+
+void *recv_msg(void *arg)
+{
+	int sock=*((int*)arg);
+	char *com[100], command[BUFSIZ];
+	int str_len, pid, i=0;
+
+	while(1)
+	{
+		for(i=i; i>=0; i--)
+			com[i] = 0;
+		
+		str_len = read(sock, command, BUFSIZ);
+		if(str_len == -1) 
+			oops("read() error");
+		command[str_len] = 0; //strlen-1 -> strlen
+		
+		if(!strcmp(command, "q"))
+			exit(1);
+
+		i=0;
+		com[i] = strtok(command, " ");
+		while(com[i] != NULL)
+			com[++i] = strtok(NULL, " ");
+
+		pid = fork();
+		if(pid == 0)
+			execvp(com[0], com);
+	}
+	return NULL;
 }
--- server.c
+++ server.c
@@ -5,20 +5,31 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <ctype.h>
+#include <pthread.h>
 
 #define oops(msg)   {perror(msg); exit(1);}
+#define MAX_CLNT 256
+
+void * handle_clnt(void * arg);
+
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutx;
+char command[BUFSIZ];
 
 int main(int ac, char *av[])
 {
-	int serv_sock, clnt_sock, str_len;
+	int serv_sock, clnt_sock, str_len, i;
 	struct sockaddr_in serv_adr;
 	char message[BUFSIZ];		/* to receive message	*/
+	pthread_t t_id;
 
-	if(ac != 3) {
-		printf("Usage : %s <PORT> <DIRECTORY>\n", av[0]);
+	if(ac != 2) {
+		printf("[USAGE] %s <PORT>\n", av[0]);
 		exit(1);
 	}
 
+	pthread_mutex_init(&mutx, NULL);
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	if(serv_sock == -1)
 		oops("socket() error");
@@ -34,22 +45,73 @@ int main(int ac, char *av[])
 	if(listen(serv_sock, 10) != 0)
 		oops("listen() error");
 
-	while(1) {
+	fputs("[INPUT] Input command: ", stdout);
+	fgets(command, BUFSIZ, stdin);
+	for(i=0; i < BUFSIZ ; i++){
+		if(command[i] == '\n'){
+			command[i] = 0;
+			break;
+		}
+	}
+	printf("[DEBUG] Command is %s\n", command);
+	fflush(stdout);
+	fputs("-------------------------------\n", stdout);
+	fflush(stdout);
+
+	fputs("[NOTICE] Waiting for clients...\n", stdout);
+	fflush(stdout);
+
+	while(1)
+	{
 		clnt_sock = accept(serv_sock, NULL, NULL);
-		if(clnt_sock == -1)
+		if( clnt_sock == -1)
 			oops("accept() error");
 
-		if(write(clnt_sock, av[2], strlen(av[2])) == -1)
-			oops("write() error");
-		if(write(clnt_sock, "\n", 1) == -1)
-			oops("write() error");
+		pthread_mutex_lock(&mutx);
+		clnt_socks[clnt_cnt++] = clnt_sock;
+		pthread_mutex_unlock(&mutx);
 
-		while((str_len = read(clnt_sock, message, BUFSIZ)) > 0) {
-			if( write(1, message, str_len) == -1)
-				oops("write");
-		}
-		close(clnt_sock);
+		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
+		pthread_detach(t_id);
+
+		printf("[NOTICE] New client is connected! - %d\n", clnt_sock);
+		fflush(stdout);
 	}
+
 	close(serv_sock);
 	return 0;
+}
+
+void * handle_clnt(void * arg)
+{
+	int clnt_sock = *((int*)arg);
+	int str_len=0, i;
+	//char command[BUFSIZ];
+	char message[BUFSIZ];
+
+	if( write(clnt_sock, command, strlen(command)) == -1)
+		oops("write command  error");
+
+	//get result
+	while((str_len = read(clnt_sock, message, BUFSIZ)) != 0)
+		write(1, message, str_len);
+
+	//remove disconnected client
+	pthread_mutex_lock(&mutx);
+	for( i = 0 ; i < clnt_cnt ; i++)
+	{
+		if(clnt_sock == clnt_socks[i])
+		{
+			while( i < clnt_cnt){
+				clnt_socks[i] = clnt_socks[i+1];
+				i++;
+			}
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutx);
+	
+	close(clnt_sock);
+	return NULL;
 }
